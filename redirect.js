9c9610193872c6596b41f6a085e24f3b5742f4ed
boccos <145885073+boccos@users.noreply.github.com>
2025-09-12 02:47:18 -0400

    Added the chat page and a simple js file for redir
    The redir from the js file is for the index page
    
    The chat page has a css file that matches the layout
    and format of the other pages

---
+++ redirect.js
@@ -0,0 +1,8 @@
+function redir(buttonId){
+    const button = document.getElementById(buttonId);
+    if (button){
+        button.addEventListener('click', () => window.location.href = "chat.html");
+    }
+}
+
+redir("chatRedir");
